553b64342d1485459fba5edd51017218c1f08b19
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2018-02-20 15:51:11 -0300

    Update Application.java

--- src/main/java/bootwildfly/Application.java
+++ src/main/java/bootwildfly/Application.java
@@ -14,12 +14,42 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
+    @Autowired
+	private CustomerRepository repository;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        ////////////////////////////
+        this.repository.deleteAll();
+
+        this.repository.save(new Customer(UUIDs.timeBased(), "Alice", "Smith"));
+		this.repository.save(new Customer(UUIDs.timeBased(), "Bob", "Smith"));
+
+		// fetch all customers
+		System.out.println("Customers found with findAll():");
+		System.out.println("-------------------------------");
+		for (Customer customer : this.repository.findAll()) {
+			System.out.println(customer);
+		}
+		System.out.println();
+
+		// fetch an individual customer
+		System.out.println("Customer found with findByFirstName('Alice'):");
+		System.out.println("--------------------------------");
+		System.out.println(this.repository.findByFirstName("Alice"));
+
+		System.out.println("Customers found with findByLastName('Smith'):");
+		System.out.println("--------------------------------");
+		for (Customer customer : this.repository.findByLastName("Smith")) {
+			System.out.println(customer);
+		}
+        ////////////////////////////
+        
+        
         return application.sources(Application.class);
     }
 }

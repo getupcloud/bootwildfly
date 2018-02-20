20f38dbe32a58625baa6c96062fd8b03a304fd8b
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2018-02-20 16:02:37 -0300

    Update Customer.java

--- src/main/java/bootwildfly/Customer.java
+++ src/main/java/bootwildfly/Customer.java
@@ -2,8 +2,8 @@ package bootwildfly;
 
 import java.util.UUID;
 
-import org.springframework.data.cassandra.core.mapping.PrimaryKey;
-import org.springframework.data.cassandra.core.mapping.Table;
+import org.springframework.data.cassandra.mapping.PrimaryKey;
+import org.springframework.data.cassandra.mapping.Table;
 
 @Table
 public class Customer {
